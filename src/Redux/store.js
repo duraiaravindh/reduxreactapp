f5f21b98b060d0a4879de3fc72a5f4f6e6d8d1ae
Durai Aravindhraj Elangovan <83654766+duraiaravindh@users.noreply.github.com>
2024-05-11 23:25:14 +0530

    Add files via upload

---
+++ src/Redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "../Redux/cart";
+
+
+export const store = configureStore({
+  reducer: {
+    cartData: cartReducer,
+  },
+});
---
+++ src/Route/AllRoute.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AllRoute() {
+  return (
+    <div>AllRoute</div>
+  )
+}
+
+export default AllRoute
